125heracles123/shopify-vite

commit da58651626edc9a3c760fe860400146fa6a93e09

Refactor config and html plugins

packages/vite-plugin-shopify/src/config.ts
@@ -1,25 +1,42 @@
-import { posix, basename, join } from 'path'
-import { ENTRYPOINT_TYPES_REGEX } from './constants'
-import type { OutputOptions, PreRenderedChunk, PreRenderedAsset } from 'rollup'
-import { Entrypoints } from './types'
+import path from 'path'
+import { Plugin, UserConfig } from 'vite'
+import glob from 'fast-glob'
 
-export function filterEntrypointsForRollup (entrypoints: Entrypoints): Entrypoints {
-  return entrypoints.filter(([_name, filename]) => ENTRYPOINT_TYPES_REGEX.test(filename))
-}
+import { ResolvedVitePluginShopifyOptions } from './options'
 
-export function outputOptions (assetsDir: string): OutputOptions {
-  const outputFileName = (ext: string) => (chunkInfo: PreRenderedChunk | PreRenderedAsset) => {
-    const shortName = basename(chunkInfo.name as string).split('.')[0]
-    return posix.join(assetsDir, `${shortName}.[hash].${ext}`)
-  }
-
+// Plugin for setting necessary Vite config to support Shopify plugin functionality
+export default function shopifyConfig (options: ResolvedVitePluginShopifyOptions): Plugin {
   return {
-    entryFileNames: outputFileName('js'),
-    chunkFileNames: outputFileName('js'),
-    assetFileNames: outputFileName('[ext]')
+    name: 'vite-plugin-shopify-config',
+    config () {
+      const generatedConfig: UserConfig = {
+        base: '',
+        // Do not use public directory
+        publicDir: false,
+        build: {
+          // Output static files to "assets" directory
+          outDir: path.join(options.themeRoot, 'assets'),
+          // Do not use subfolder for output assets
+          assetsDir: '',
+          // Clear output directory before each build
+          emptyOutDir: true,
+          // Configure bundle entry points
+          rollupOptions: {
+            input: glob.sync(path.join(options.entrypointsDir, '**.*'), { onlyFiles: true })
+          }
+        },
+        resolve: {
+          alias: {
+            '~': options.themeRoot,
+            '@': options.themeRoot
+          }
+        },
+        server: {
+          host: true
+        }
+      }
+
+      return generatedConfig
+    }
   }
 }
-
-export const projectRoot = process.cwd()
-export const sourceCodeDir = join(projectRoot, 'frontend')
-export const root = join(sourceCodeDir, 'entrypoints')

packages/example/assets/theme.0d34937e.js
@@ -1,2 +1,0 @@
-const f=window.themeAssetsBaseUrl,a="modulepreload",l={},d=f,m=function(n,i){return!i||i.length===0?n():Promise.all(i.map(r=>{if(r=`${d}${r}`,r in l)return;l[r]=!0;const e=r.endsWith(".css"),t=e?'[rel="stylesheet"]':"";if(document.querySelector(`link[href="${r}"]${t}`))return;const o=document.createElement("link");if(o.rel=e?"stylesheet":a,e||(o.as="script",o.crossOrigin=""),o.href=r,document.head.appendChild(o),e)return new Promise((c,u)=>{o.addEventListener("load",c),o.addEventListener("error",()=>u(new Error(`Unable to preload CSS for ${r}`)))})})).then(()=>n())},h=function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&r(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}};h();m(()=>import("./lodash.9a6714d2.js"),[]).then(({default:s})=>{console.log(s.join(["Vite","\u26A1\uFE0F","Shopify"]," "))}).catch(s=>console.log(s));
-//# sourceMappingURL=theme.0d34937e.js.map

packages/example/assets/theme.0d34937e.js.map
@@ -1,1 +1,0 @@
-{"version":3,"mappings":"AAAA,yDAAiF,oBAEjF,uBACA,IAEA,sBAIA,GAFA,aAEA,OACA,OAEA,QACA,2BACA,4BAEA,mDACA,OAGA,uCAUA,GARA,uBACA,GACA,eACA,kBAEA,SAEA,6BACA,EACA,2BACA,6BACA,8EACA,EAEA,iBACA,ECrCM,EAAI,UAAoB,CAC1B,KAAM,GAAU,SAAS,cAAc,MAAM,EAAE,QAC/C,GAAI,GAAW,EAAQ,UAAY,EAAQ,SAAS,eAAe,EAC/D,OAEJ,SAAW,KAAQ,UAAS,iBAAiB,2BAA2B,EACpE,EAAe,CAAI,EAEvB,GAAI,kBAAiB,CAAC,EAAc,CAChC,SAAW,KAAY,GACnB,GAAI,EAAS,OAAS,YAGtB,SAAW,KAAQ,GAAS,WACxB,CAAI,CAAK,UAAY,QAAU,EAAK,MAAQ,iBACxC,EAAe,CAAI,CAGvC,CAAK,EAAE,QAAQ,SAAU,CAAE,UAAW,GAAM,QAAS,EAAI,CAAE,EACvD,WAAsB,EAAQ,CAC1B,KAAM,GAAY,GAClB,MAAI,GAAO,WACP,GAAU,UAAY,EAAO,WAC7B,EAAO,gBACP,GAAU,eAAiB,EAAO,gBACtC,CAAI,CAAO,cAAgB,kBACvB,EAAU,YAAc,UACvB,CAAI,CAAO,cAAgB,YAC5B,EAAU,YAAc,OAExB,EAAU,YAAc,cACrB,CACV,CACD,WAAwB,EAAM,CAC1B,GAAI,EAAK,GAEL,OACJ,EAAK,GAAK,GAEV,KAAM,GAAY,EAAa,CAAI,EACnC,MAAM,EAAK,KAAM,CAAS,CAC7B,CACL,EAAE,CAAoB,CAAG,ECxCzB,EACG,4CAAK,CAAC,CAAE,QAAS,KAAQ,CAChB,YAAI,EAAE,KAAK,CAAC,OAAQ,eAAM,SAAS,EAAG,GAAG,CAAC,CACpD,CAAC,EACA,MAAM,GAAS,QAAQ,IAAI,CAAK,CAAC","names":[],"sources":["../vite/preload-helper","../vite/modulepreload-polyfill","../frontend/entrypoints/theme.ts"],"sourcesContent":["const scriptRel = 'modulepreload';const seen = {};const base = '/__THEME_BASE__/';export const __vitePreload = function preload(baseModule, deps) {\n    // @ts-ignore\n    if (!__VITE_IS_MODERN__ || !deps || deps.length === 0) {\n        return baseModule();\n    }\n    return Promise.all(deps.map((dep) => {\n        // @ts-ignore\n        dep = `${base}${dep}`;\n        // @ts-ignore\n        if (dep in seen)\n            return;\n        // @ts-ignore\n        seen[dep] = true;\n        const isCss = dep.endsWith('.css');\n        const cssSelector = isCss ? '[rel=\"stylesheet\"]' : '';\n        // @ts-ignore check if the file is already preloaded by SSR markup\n        if (document.querySelector(`link[href=\"${dep}\"]${cssSelector}`)) {\n            return;\n        }\n        // @ts-ignore\n        const link = document.createElement('link');\n        // @ts-ignore\n        link.rel = isCss ? 'stylesheet' : scriptRel;\n        if (!isCss) {\n            link.as = 'script';\n            link.crossOrigin = '';\n        }\n        link.href = dep;\n        // @ts-ignore\n        document.head.appendChild(link);\n        if (isCss) {\n            return new Promise((res, rej) => {\n                link.addEventListener('load', res);\n                link.addEventListener('error', () => rej(new Error(`Unable to preload CSS for ${dep}`)));\n            });\n        }\n    })).then(() => baseModule());\n}","const p = function polyfill() {\n    const relList = document.createElement('link').relList;\n    if (relList && relList.supports && relList.supports('modulepreload')) {\n        return;\n    }\n    for (const link of document.querySelectorAll('link[rel=\"modulepreload\"]')) {\n        processPreload(link);\n    }\n    new MutationObserver((mutations) => {\n        for (const mutation of mutations) {\n            if (mutation.type !== 'childList') {\n                continue;\n            }\n            for (const node of mutation.addedNodes) {\n                if (node.tagName === 'LINK' && node.rel === 'modulepreload')\n                    processPreload(node);\n            }\n        }\n    }).observe(document, { childList: true, subtree: true });\n    function getFetchOpts(script) {\n        const fetchOpts = {};\n        if (script.integrity)\n            fetchOpts.integrity = script.integrity;\n        if (script.referrerpolicy)\n            fetchOpts.referrerPolicy = script.referrerpolicy;\n        if (script.crossorigin === 'use-credentials')\n            fetchOpts.credentials = 'include';\n        else if (script.crossorigin === 'anonymous')\n            fetchOpts.credentials = 'omit';\n        else\n            fetchOpts.credentials = 'same-origin';\n        return fetchOpts;\n    }\n    function processPreload(link) {\n        if (link.ep)\n            // ep marker = processed\n            return;\n        link.ep = true;\n        // prepopulate the load record\n        const fetchOpts = getFetchOpts(link);\n        fetch(link.href, fetchOpts);\n    }\n};__VITE_IS_MODERN__&&p();","import 'vite/modulepreload-polyfill'\n\nimport('lodash-es')\n  .then(({ default: _ }) => {\n    console.log(_.join(['Vite', '⚡️', 'Shopify'], ' '))\n  })\n  .catch(error => console.log(error))\n"],"file":"theme.0d34937e.js"}

packages/example/assets/theme.4e5771e8.js
@@ -1,0 +1,1 @@
+const a="modulepreload",d=function(o,r){return new URL(o,r).href},c={},m=function(r,i,l){return!i||i.length===0?r():Promise.all(i.map(e=>{if(e=d(e,l),e in c)return;c[e]=!0;const t=e.endsWith(".css"),s=t?'[rel="stylesheet"]':"";if(document.querySelector(`link[href="${e}"]${s}`))return;const n=document.createElement("link");if(n.rel=t?"stylesheet":a,t||(n.as="script",n.crossOrigin=""),n.href=e,document.head.appendChild(n),t)return new Promise((u,f)=>{n.addEventListener("load",u),n.addEventListener("error",()=>f(new Error(`Unable to preload CSS for ${e}`)))})})).then(()=>r())},h=function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}};h();m(()=>import("./lodash-es.6d048829.js"),[],import.meta.url).then(({default:o})=>{console.log(o.join(["Vite","\u26A1\uFE0F","Shopify"]," "))}).catch(o=>console.log(o));

commit 814aefae934562b8ffa4d2a72eb094355be9bcd7

Production source maps Fix
It broke in da58651

packages/vite-plugin-shopify/src/config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { Plugin, UserConfig } from 'vite'
+import { Plugin, UserConfig, ConfigEnv } from 'vite'
 import glob from 'fast-glob'
 import createDebugger from 'debug'
 
@@ -13,14 +13,15 @@ export default function shopifyConfig (options: ResolvedVitePluginShopifyOptions
 
   return {
     name: 'vite-plugin-shopify-config',
-    config (config: UserConfig): UserConfig {
+    config (config: UserConfig, env: ConfigEnv): UserConfig {
       const host = config.server?.host ?? 'localhost'
       const port = config.server?.port ?? 5173
       const https = config?.server?.https
       const protocol = https === true ? 'https:' : 'http:'
       const origin = `${protocol}//${host as string}:${port}`
+      const sourcemap = env.command === 'build'
 
-      debug({ host, port, https, protocol, origin })
+      debug({ host, port, https, protocol, origin, sourcemap })
 
       const generatedConfig: UserConfig = {
         // Use relative base path so to load imported assets from Shopify CDN
@@ -39,7 +40,9 @@ export default function shopifyConfig (options: ResolvedVitePluginShopifyOptions
             input: glob.sync(path.join(options.entrypointsDir, '**/*'), { onlyFiles: true })
           },
           // Output manifest file for backend integration
-          manifest: true
+          manifest: true,
+          // Generate production source maps
+          sourcemap
         },
         resolve: {
           // Provide import alias to source code dir for convenience